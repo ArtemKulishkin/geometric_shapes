25f210be73c48c2fa8c1093730a44e477206bd29
ArtemKulishkin <artcool163@gmail.com>
2022-03-24 11:15:09 +0700

    Обработка ошибки открытия картинки

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def find_contours(img, color):
     return contours
 
 img = cv2.imread("pool_two_bins.jpg")
+if img.any() == None:
+    print("Не найдена картинка")
+    exit()
 drawing = img.copy()
 color = (
           (30, 80, 0),
@@ -27,14 +30,14 @@ if contours:
             (circle_x, circle_y), circle_radius = cv2.minEnclosingCircle(cnt)
             circle_area = math.pi * circle_radius**2
             print("Площадь окружности:", circle_area)
-            cv2.circle(drawing, (int(circle_x), int(circle_y)), int(circle_radius), (255, 255, 0), 2)
+            
 
             rectangle = cv2.minAreaRect(cnt)
             box = cv2.boxPoints(rectangle)
             box = np.int0(box)
             rectangle_area = cv2.contourArea(box)
             print("Площадь прямоугольника:", rectangle_area)
-            cv2.drawContours(drawing, [box], 0, (0, 150, 255), 2)
+           
             rect_w, rect_h = rectangle[1][0], rectangle[1][1]
             aspect_ratio = max(rect_w, rect_h) / min(rect_w, rect_h)
 
@@ -42,7 +45,7 @@ if contours:
                 triangle = cv2.minEnclosingTriangle(cnt)[1]
                 triangle = np.int0(triangle)
                 triangle_area = cv2.contourArea(triangle)
-                cv2.drawContours(drawing, [triangle], -1, (100, 255, 155), 2)
+                
             except:
                 triangle_area = 0
 
@@ -59,8 +62,26 @@ if contours:
             diffs = {
                 name: abs(contour_area - shapes_areas[name]) for name in shapes_areas
             }
-            print()
+            shape_name = min(diffs, key=diffs.get)
+            if shape_name == "triangle":
+                cv2.drawContours(drawing, [triangle], -1, (100, 255, 155), 2)
+            elif shape_name == "rectangle" or shape_name == "square":
+                cv2.drawContours(drawing, [box], 0, (0, 150, 255), 2)
+            else:
+                cv2.circle(drawing, (int(circle_x), int(circle_y)), int(circle_radius), (255, 255, 0), 2)
+            
+            moments = cv2.moments(cnt)
 
+            try:
+                x = int(moments['m10']/moments['m00'])
+                y = int(moments['m01']/moments['m00'])
+                cv2.circle(drawing, (x, y), 4, (0, 100, 255), -1, cv2.LINE_AA)
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(drawing, shape_name, (x-40, y+31), font, 1, (0, 0, 0), 4, cv2.LINE_AA)
+                cv2.putText(drawing, shape_name, (x-41, y+30), font, 1, (255, 255, 255), 4, cv2.LINE_AA)
+            except ZeroDivisionError:
+                pass
+            print()
             
 cv2.imshow("drawing", drawing)
 cv2.waitKey(0)
